dd277b2a4fc937eb0d7230e7405f5ac3573074ea
Gohulan Somanathan <sgohul.24@gmail.com>
2020-03-29 09:00:10 +0530

    Add files via upload

---
+++ Splash Screen nTest/MainScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Splash_Screen_nTest
+{
+	public partial class MainScreen : Form
+	{
+		public MainScreen()
+		{
+			Thread t = new Thread(new ThreadStart(StartForm));
+			t.Start();
+			Thread.Sleep(5000);
+			InitializeComponent();
+			t.Abort();
+		}
+
+		public void StartForm()
+		{
+			Application.Run(new SplashScreen());
+		}
+
+		private void MainScreen_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ Splash Screen nTest/SplashScreen.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Splash_Screen_nTest
+{
+	partial class SplashScreen
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Splash_Screen_nTest.Properties.Resources._27067584_2007014579586259_2644014414439330757_n;
+			this.pictureBox1.Location = new System.Drawing.Point(-84, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(563, 209);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(23, 227);
+			this.progressBar1.MarqueeAnimationSpeed = 50;
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(350, 16);
+			this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+			this.progressBar1.TabIndex = 1;
+			// 
+			// SplashScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(401, 272);
+			this.ControlBox = false;
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "SplashScreen";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "SplashScreen";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+	}
+}
